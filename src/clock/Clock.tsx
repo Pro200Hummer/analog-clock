cfac609dd13b17859fb65f1d8b09495bf1aca308
Pavel <nikolaj4ik.pasha@yandex.ru>
2021-11-08 23:09:41 +0300

    Add clock logic

--- src/clock/Clock.tsx
+++ src/clock/Clock.tsx
@@ -8,8 +8,20 @@ export class Clock extends Component<{}, StateType> {
     constructor(props: {}) {
         super(props);
         this.state = {
-            currentTime: (new Date().toLocaleString())
+            currentTime: (new Date()).toLocaleString()
         }
+    }
+
+    componentDidMount() {
+        this.launchClock();
+    }
+
+    launchClock() {
+        setInterval(() => {
+            this.setState({
+                currentTime: (new Date()).toLocaleString()
+            })
+        }, 1000)
     }
 
     render() {
