c99c377d45908b9a40a924646dee375856943346
Pavel <nikolaj4ik.pasha@yandex.ru>
2021-11-08 22:26:57 +0300

    Component customization

---
+++ src/clock/Clock.tsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+
+export class Clock extends Component<{}, {}>{
+    render() {
+        return <div>
+            Hello! I'm clock!
+        </div>;
+    }
+}
